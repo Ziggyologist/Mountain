697a6120d477656e6b9d47633211b10fd5d4cf36
Ziggyologist <teodora11vela@gmail.com>
2020-10-04 01:18:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let culoareaAleasa;
+
+const culori= {
+    "bad": "#556B2F",
+    "ok": "#9ACD32",
+    "good": "#f8de7e",
+    "awesome":"#fffe7a",
+}
+
+function coloreaza(id){
+    document.getElementById(id.id).style.fill = culoareaAleasa;
+}
+
+const changeColor = (color) =>{
+    if(color === 'bad'){
+        culoareaAleasa = culori.bad;
+    } 
+    if(color==="ok"){
+        culoareaAleasa=culori.ok;
+    }
+    if(color==="good"){
+        culoareaAleasa=culori.good;
+    }
+    if(color==="awesome"){
+        culoareaAleasa=culori.awesome;
+    }
+}
+
+
+for (let i in document.querySelectorAll('polygon')) {
+    document.querySelectorAll('polygon')[i].addEventListener('click', () => {
+        coloreaza(document.querySelectorAll('polygon')[i]);
+    })
+}
